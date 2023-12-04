63be9705b347ac39aca4e941a88be3129b62b9b4
Tonmoy <shahariartonmoy17@gmail.com>
2023-12-04 14:21:59 +0600

    api for deleting review

--- index.js
+++ index.js
@@ -192,6 +192,15 @@ async function run() {
         const result = await WishlistCollection.deleteOne(query);
         res.send(result);
       })
+    app.delete('/reviews/:cid', async(req, res) => {
+        const id  = req.params.cid;
+        console.log(`PLEASE DELETE ID FROM DATABASE: ${id}`);
+        const query = { _id: new ObjectId(id)};
+        console.log(query);
+        
+        const result = await ReviewsCollection.deleteOne(query);
+        res.send(result);
+      })
 
     //api for update data
     app.patch('/properties/:id', async(req, res) =>{
